87cdfa527925d7bb86ed4d0fde890cfb2a160b73
Devtechk <antoninogiuliano.mail@gmail.com>
2023-11-10 13:48:31 +0000

    test on version

--- dispenserevolver.js
+++ dispenserevolver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dispense revoler downloader
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  Apre i link di tutte le dispense in una pagina nuova prima controlla che il captcha sia risolto
 // @author       You
 // @match    https://lms-courses.pegaso.multiversity.click/main/lp-video_student_view/*
